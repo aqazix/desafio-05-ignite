b0e7dfa3e2ea385b12797b071ed1f54763fbcc0f
Pedro Sousa <pedrorrdesousa97@gmail.com>
2021-07-28 10:39:32 -0300

    Initial commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const users = this.usersRepository.list();
+        const user = this.usersRepository.findById(user_id);
+
+        if (user) {
+            if (user.admin) return users;
+
+            throw new Error("Usuário não é um admin");
+        }
+
+        throw new Error("Usuário não encontrado com o ID fornecido");
+    }
 }
 
 export { ListAllUsersUseCase };
